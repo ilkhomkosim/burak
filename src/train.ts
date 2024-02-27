792b8a16f9f26dc9ec22e522595ec5e95ebffcd8
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-02-27 22:38:51 +0900

    feat: create routing and member controller + train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,22 @@
-function getReverse(String: string): string {
-    return String.split('').reverse().join('');
+// function getReverse(String: string): string {
+//     return String.split('').reverse().join('');
+// }
+
+// const reverse: string = getReverse("hello");
+// console.log(reverse); 
+
+// Task_H
+
+function Positive(numbers: number[]): string {
+    const positiveNumbers: number[] = numbers.filter(num => num > 0);
+    const positiveNumbersAsString: string = positiveNumbers.join('');
+    return positiveNumbersAsString;
 }
 
-const reverse: string = getReverse("hello");
-console.log(reverse); 
+// Masalan:
+const result: string = Positive([1, -4, 2]);
+console.log(result);
+
+
 
 
