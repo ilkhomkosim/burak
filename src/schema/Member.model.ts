6d4e8d09c47275cd86bac1c244c37eeba90dc74b
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-01 13:40:15 +0900

    feat: develop singup process

--- src/schema/Member.model.ts
+++ src/schema/Member.model.ts
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import { MemberStatus, MemberType } from "..//libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 // schema based and code based
 const memberSchema = new Schema({
     memberType: {
