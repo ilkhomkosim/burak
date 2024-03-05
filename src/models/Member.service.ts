31e135e4c8a29ca3d3e368918538a976b323abf6
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-06 08:47:39 +0900

    feat:admin signup/login & bcrypt

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,8 @@
 import  MemberModel  from "../schema/Member.model";
-import {Member, MemberInput} from "../libs/types/member";
+import {LoginInput, Member, MemberInput} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -16,6 +17,9 @@ class MemberService {
         .exec();
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+        
         try{
             const result = await this.memberModel.create(input);
         result.memberPassword = "";
@@ -23,6 +27,31 @@ class MemberService {
         } catch (err) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
+    }
+
+    public async processLogin(input: LoginInput): Promise<Member> {
+        const member = await this.memberModel
+        .findOne(
+        {memberNick: input.memberNick},
+        {memberNick: 1 , memberPassword: 1}
+        )
+        .exec();
+        if(!member) throw new Errors (HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = await bcrypt.compare(
+            input.memberPassword,
+            member.memberPassword
+        );
+        // const isMatch = input.memberPassword === member.memberPassword;
+
+        
+
+        if (!isMatch) {
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
+        }
+
+        return await this.memberModel.findById(member._id).exec();
+        
     }
 }
 
--- src/train.ts
+++ src/train.ts
@@ -7,15 +7,29 @@
 
 // Task_H
 
-function Positive(numbers: number[]): string {
-    const positiveNumbers: number[] = numbers.filter(num => num > 0);
-    const positiveNumbersAsString: string = positiveNumbers.join('');
-    return positiveNumbersAsString;
+// function Positive(numbers: number[]): string {
+//     const positiveNumbers: number[] = numbers.filter(num => num > 0);
+//     const positiveNumbersAsString: string = positiveNumbers.join('');
+//     return positiveNumbersAsString;
+// }
+
+// // Masalan:
+// const result: string = Positive([1, -4, 2]);
+// console.log(result);
+
+
+// TASK_I
+
+function getDigits(inputString: string): string {
+    const digitsArray: RegExpMatchArray | null = inputString.match(/\d/g);
+
+    const resultString: string = digitsArray ? digitsArray.join('') : '';
+
+    return resultString;
 }
 
-// Masalan:
-const result: string = Positive([1, -4, 2]);
-console.log(result);
+const result: string = getDigits("m14i1t");
+console.log(result); 
 
 
 
