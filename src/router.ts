ce76accad557a8eadb374c03c97b7f58feb628e7
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-06 12:08:03 +0900

    fix: organize spa & ssrbusiness logic

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,9 @@ import express from "express";
 const router = express.Router();
 import memberController from './controllers/member.controller';
 
-// router.get("/", memberController.goHome);
+router.post("/login/", memberController.login);
+router.post("/signup", memberController.signup);
 
-// router.get("/login", memberController.getLogin);
 
-// router.get("/signup", memberController.getSignup);
 
 export default router;
