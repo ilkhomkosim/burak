fa361d987bf6fd41cbbbd36d119bdee761bd09d2
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-10 23:41:03 +0900

    feat: configuration of  product Schema model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,28 @@
+export enum ProductSize{
+    SMALL = "SMALL",
+    NORMAL = "NORMAL",
+    LARGE = "LARGE",
+    SET = "SET"
+}
+
+export enum Productvolume{
+    HALF = 0.5,
+    ONE = 1,
+    ONE_POINT_TWO = 1.2,
+    ONE_POINT_FIVE = 1.5,
+    TWO = 2,
+}
+
+export enum ProductStatus {
+    PAUSE ="PAUSE",
+    PROCESS = "PROCESS",
+    DELETE = "DELETE"
+}
+
+export enum ProducCollection {
+    PAUSE = "PAUSE",
+    SALAD = "SALAD",
+    DESSERT = "DESERT",
+    DRINK = "DRINK",
+    OTHER = "OTHER"
+}
---
+++ src/schema/Product.model.ts
@@ -0,0 +1,73 @@
+import mongoose, {Schema} from "mongoose";
+import { 
+    ProducCollection, 
+    ProductSize,
+    ProductStatus, 
+    Productvolume 
+} from "../libs/enums/product.enum";
+
+const productSchema = new Schema(
+{
+    productStatus: {
+        type: String,
+        enum: ProductStatus,
+        default: ProductStatus.PAUSE,
+    },
+
+    productCollection: {
+        type: String,
+        enum: ProducCollection,
+        required: true,
+    },
+
+    productName: {
+        type: String,
+        required: true,
+    },
+
+    productPrice: {
+        type:Number,
+        required: true,
+    },
+
+    productLeftCount: {
+        type: Number,
+        required: true,
+    },
+
+    productSize: {
+        type: String,
+        enum: ProductSize,
+        required: true,
+    },
+
+    productVolume: {
+        type: String,
+        enum: Productvolume,
+        default:Productvolume.ONE,
+    },
+
+    productDesc: {
+        type: String,
+        required: true,
+    },
+
+    productImage: {
+        type: [String],
+        default: [],
+    },
+
+    productViews: {
+        type:Number,
+        default:0,
+    }
+
+},
+{ timestamps: true}  // updateAt, createAt
+);
+
+
+productSchema.index({productNane: 1, ProductSize:1, Productvolume:1},
+    {unique:true}
+    );
+export default mongoose.model("Product", productSchema)
