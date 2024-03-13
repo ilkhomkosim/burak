f4280de61babcd9f05940b59ac61128e99098fc3
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-13 21:50:16 +0900

    fix: modify restaurant controller processSignup and processLogin

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -3,6 +3,7 @@ const routerAdmin = express.Router();
 import restaurantController from './controllers/restaurant.controller';
 import productController from "./controllers/product.controller";
 import makeUploader from "./libs/utils/uploader";
+
 /** Restaurant*/
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
@@ -13,21 +14,25 @@ routerAdmin
     .post(
         "/signup",
         makeUploader("members").single("memberImage"),
-        restaurantController.processSignup);
+        restaurantController.processSignup
+        );
     routerAdmin.get("/logout", restaurantController.logout);
     routerAdmin.get("/check-me", restaurantController.checkAuthSession)
     
     /** Product */
-    routerAdmin.get("/product/all", 
+    routerAdmin.get(
+        "/product/all", 
     restaurantController.verifyRestaurant,
     productController.getAllProducts
     );
-    routerAdmin.post("/product/create",
+    routerAdmin.post(
+        "/product/create",
     restaurantController.verifyRestaurant, 
     makeUploader("products").array("productImages", 5),
     productController.createNewProduct
     );
-    routerAdmin.post("/product/:id", 
+    routerAdmin.post(
+        "/product/:id", 
     restaurantController.verifyRestaurant, 
     productController.updateChosenProduct
     );
