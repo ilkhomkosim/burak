55a5a3a23ee495b5b5efde4087467489d8f23417
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-03-13 19:19:52 +0900

    fix: app.ts restored

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import  express from "express";
+import path from "path";
+import router from "./router";
+import routerAdmin from "./router-admin";
+import morgan from "morgan";
+import {MORGAN_FORMAT} from "./libs/config"
+
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL),
+    collection: "sessions",
+});
+
+// 1- ENTRANCE
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
+
+// 2- SESSIONS
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),
+        cookie: {
+            maxAge: 1000*3600*10,  // 6h
+        },
+        store: store,
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.use(function(req, res, next) {
+    const sessionInstance = req.session as T;
+    res.locals.member = sessionInstance.member;
+    next();
+});
+
+// 3-VIEWS
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+// 4-ROUTERS
+app.use("/admin", routerAdmin)     // EJS 
+app.use("/", router);              // REACT 
+
+
+
+
+export default app;
+
+
